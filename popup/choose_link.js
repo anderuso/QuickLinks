38404f9317199a2a579ba19d2efac6c82608de65
anderuso <21160689+anderuso@users.noreply.github.com>
2017-10-03 20:14:25 +0200

    Shown item from options instead of static list. Removed Reset menu item and replaced by Show options

--- popup/choose_link.js
+++ popup/choose_link.js
@@ -6,6 +6,30 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
+function createLink(linkUrl) {
+	var linkDiv = document.createElement('div');
+	linkDiv.setAttribute("class", "button site");
+	linkDiv.textContent = linkUrl;
+	
+	return linkDiv;
+}
+
+function onGot(item) {
+	var linksDiv = document.getElementById("links");
+	if (item.link)
+	{
+		var optionLink = createLink(item.link)
+	}
+	else
+	{
+		var optionLink = createLink("Nothing found in options")
+	}
+	linksDiv.appendChild(optionLink);
+}
+
+var getting = browser.storage.local.get("link");
+getting.then(onGot, onError);
+
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("site")) {
     var chosenBeastURL = e.target.textContent;
@@ -13,8 +37,6 @@ document.addEventListener("click", (e) => {
 	var creating = browser.tabs.create({url: "https://" + chosenBeastURL});
 	creating.then(onCreated, onError);
   }
-  else if (e.target.classList.contains("clear")) {
-    browser.tabs.reload();
-    window.close();
-  }
+  else if (e.target.classList.contains("options")) {
+	browser.runtime.openOptionsPage()  }
 });
